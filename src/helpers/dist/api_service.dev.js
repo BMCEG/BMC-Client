f9f08c3316695c2fc43b07df0143d77d9668070e
Ali Moneib <ali_moneib@yahoo.com>
2021-07-27 12:31:28 +0200

    fixes

--- src/helpers/dist/api_service.dev.js
+++ src/helpers/dist/api_service.dev.js
@@ -1,5 +1,5 @@
 "use strict";
 
-var endpoint = 'http://207.154.254.236:5000/api'; // const endpoint = 'http://localhost:5000/api';
+var endpoint = 'http://207.154.254.236/api'; // const endpoint = 'http://localhost:5000/api';
 
 module.exports = endpoint;
